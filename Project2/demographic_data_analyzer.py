7585b9d90ad99b165938d5c8ed99127168f06004
Rasyid <189116968+RsydMathTech@users.noreply.github.com>
2025-08-14 16:06:21 +0700

    Update demographic_data_analyzer.py

--- Project2/demographic_data_analyzer.py
+++ Project2/demographic_data_analyzer.py
@@ -1,51 +1,40 @@
 import pandas as pd
 
 def calculate_demographic_data(print_data=True):
-    # Baca dataset
+    
     df = pd.read_csv("adult.data", header=None, names=[
         "age", "workclass", "fnlwgt", "education", "education_num",
         "marital_status", "occupation", "relationship", "race", "sex",
         "capital_gain", "capital_loss", "hours_per_week", "native_country", "income"
     ], na_values=" ?", skipinitialspace=True)
 
-    # 1. Berapa orang di setiap ras
     race_count = df['race'].value_counts()
 
-    # 2. Rata-rata umur pria
     average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
 
-    # 3. Persentase orang dengan Bachelor's
     percentage_bachelors = round((df['education'] == 'Bachelors').mean() * 100, 1)
 
-    # 4. Advanced education
     higher_education = df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
     lower_education = ~higher_education
 
-    # Persentase gaji >50K untuk yang higher ed
     higher_education_rich = round((df[higher_education]['income'] == '>50K').mean() * 100, 1)
 
-    # Persentase gaji >50K untuk yang lower ed
     lower_education_rich = round((df[lower_education]['income'] == '>50K').mean() * 100, 1)
 
-    # 5. Minimum hours per week
     min_work_hours = df['hours_per_week'].min()
 
-    # 6. Persentase gaji >50K untuk orang yang kerja minimum jam
     num_min_workers = df[df['hours_per_week'] == min_work_hours]
     rich_percentage = round((num_min_workers['income'] == '>50K').mean() * 100, 1)
 
-    # 7. Negara dengan persentase >50K tertinggi
     country_earning = df[df['income'] == '>50K']['native_country'].value_counts()
     country_total = df['native_country'].value_counts()
     country_percentage = (country_earning / country_total * 100).round(1)
     highest_earning_country = country_percentage.idxmax()
     highest_earning_country_percentage = country_percentage.max()
 
-    # 8. Pekerjaan terpopuler di India untuk >50K
     india_occupation = df[(df['native_country'] == 'India') & (df['income'] == '>50K')]['occupation'].value_counts()
     top_IN_occupation = india_occupation.idxmax()
 
-    # Output
     if print_data:
         print("Number of each race:\n", race_count)
         print("Average age of men:", average_age_men)
